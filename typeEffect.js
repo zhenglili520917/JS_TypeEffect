9be56a6f23893fbb43d0b55248284735a23c58f9
KID421 <suchild7@gmail.com>
2019-09-24 21:15:29 +0800

    定義變數與測試

---
+++ typeEffect.js
@@ -0,0 +1,7 @@
+var texts = ["網頁設計師", "插畫家", "遊戲開發者"];   // 陣列
+var speed = 1000;                                   // 打字速度
+var index = 0;                                      // 編號
+var count = 0;                                      // 字數
+
+// 測試
+document.getElementById("type-box").innerHTML = texts[0];
