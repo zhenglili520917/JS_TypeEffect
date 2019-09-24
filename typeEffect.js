c06d49cd721534ee9a284cb0c4588a9b138fb0cb
KID421 <suchild7@gmail.com>
2019-09-24 21:29:55 +0800

    定義打字方法與設定網頁載入完成時執行方法

--- typeEffect.js
+++ typeEffect.js
@@ -2,6 +2,13 @@ var texts = ["網頁設計師", "插畫家", "遊戲開發者"];   // 陣列
 var speed = 1000;                                   // 打字速度
 var index = 0;                                      // 編號
 var count = 0;                                      // 字數
+var id = "type-box";                                // 要寫入文字的 ID
 
-// 測試
-document.getElementById("type-box").innerHTML = texts[0];
+// 定義方法
+function type(){
+    var box = document.getElementById(id);
+    box.innerHTML = texts[index];
+}
+
+// 當網頁載入完成時執行方法 type
+document.addEventListener("DOMContentLoaded", type);
